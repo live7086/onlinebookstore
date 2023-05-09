9716ea083d0717a901db10db18bb8029115a6b33
live7086 <luzzyyo@gmail.com>
2023-05-09 20:40:01 +0800

    product delist

--- bidstatement.php
+++ bidstatement.php
@@ -5,10 +5,10 @@ $item_id    = $_GET['item_id'];
 $bid_id     = $_GET['bid_id'];
 echo $bid_id;
 $link = mysqli_connect('localhost','root','12345678','sa');
-//更新bid_info狀態
+//更新bid_info狀態//
 $sql  = "UPDATE bid_info SET statement = '$statement' WHERE item_id = '$item_id' AND bid_id = '$bid_id' ";
 mysqli_query($link,$sql);
-//更新item_info狀態
+//更新item_info狀態//
 if($statement=="accepted"){
     $sql  = "UPDATE item_info SET statement = 'sold' WHERE item_id = '$item_id' ";
     mysqli_query($link,$sql);
